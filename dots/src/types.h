39b18ca70b710fe2f02513119616d59640684c77
rundekugel <rundekugel@gmx.de>
2020-06-13 16:25:14 +0200

    .

---
+++ dots/src/types.h
@@ -0,0 +1,20 @@
+//types
+
+
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+typedef signed char     S8;
+typedef unsigned char   U8;
+typedef short           S16;
+typedef unsigned short  U16;
+typedef int             S32;
+typedef unsigned int    U32;
+typedef long long       S64;
+typedef unsigned long long U64;
+typedef unsigned char   BIT;
+typedef unsigned int    BOOL;
+
+#endif //__TYPES_H__
+//eof
+
